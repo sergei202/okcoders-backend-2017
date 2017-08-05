d0dfea601cd072865e6fb64ba218ce294729fe0e
Sergei <sergei@sleekbook>
2017-08-05 09:53:27 -0500

    Add controllers to SPA example.

--- week7/examples/spa/app.js
+++ week7/examples/spa/app.js
@@ -3,10 +3,20 @@ var app = angular.module('spaApp', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/page1', {
-			templateUrl: 'page1.html'
+			templateUrl: 'page1.html',
+			controller: 'Page1Ctrl'
 		})
 		.when('/page2', {
-			templateUrl: 'page2.html'
+			templateUrl: 'page2.html',
+			controller: 'Page2Ctrl'
 		})
 		.otherwise('/page1');
+});
+
+app.controller('Page1Ctrl', function($scope) {
+	$scope.title = 'We are in Page1';
+});
+
+app.controller('Page2Ctrl', function($scope) {
+	$scope.title = 'We are in Page2';
 });
